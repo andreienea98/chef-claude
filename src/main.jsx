080041a9188b2ba006ae73baddc990e8b7cddb69
andreienea398 <andreihd398@gmail.com>
2025-08-29 20:57:21 -0300

    added formData and set the state

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,30 @@
+import React from "react"
+
 export default function Main() {
+  const [ingredients, setIngredients] = React.useState(["chicken", "oregano", "tomatoes"])
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ))
+
+  function handleSubmit(formData) {
+    const newIngredient = formData.get("ingredient")
+    setIngredients(prevIngredients => [...prevIngredients, formData.get("ingredient")])
+    console.log(newIngredient)
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form action={handleSubmit} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+      <ul>{ingredientsListItems}</ul>
     </main>
   )
 }
