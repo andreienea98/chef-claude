50734441d2d17400d5dade95094d9d1022f074bd
andreienea398 <andreihd398@gmail.com>
2025-09-08 21:28:52 -0300

    refactor to separate components and sharing state

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import React from "react"
+import ClaudeRecipe from "./ClaudeRecipe"
+import IngredientsList from "./IngredientsList"
 
 export default function Main() {
   const [ingredients, setIngredients] = React.useState([])
 
-  const ingredientsListItems = ingredients.map((ingredient) => (
-    <li key={ingredient}>{ingredient}</li>
-  ))
+  const [recipeShown, setRecipeShown] = React.useState(false)
+
+  function toggleRecipeShown() {
+    setRecipeShown(prevShown => !prevShown)
+  }
+
+  
 
   function handleSubmit(formData) {
     const newIngredient = formData.get("ingredient")
@@ -23,23 +29,8 @@ export default function Main() {
         />
         <button>Add ingredient</button>
       </form>
-      {ingredients.length > 0 && (
-        <section>
-          <h2>Ingredients on hand:</h2>
-          <ul className="ingredients-list" aria-live="polite">
-            {ingredientsListItems}
-          </ul>
-          {ingredients.length > 3 && (
-            <div className="get-recipe-container">
-              <div>
-                <h3>Ready for a recipe?</h3>
-                <p>Generate a recipe from your list of ingredients.</p>
-              </div>
-              <button>Get a recipe</button>
-            </div>
-          )}
-        </section>
-      )}
+      {ingredients.length > 0 && <IngredientsList ingredients={ingredients} toggle={toggleRecipeShown}/>}
+      {recipeShown && <ClaudeRecipe />}
     </main>
   )
 }
